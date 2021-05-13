4544de05b59131e5e58b48aac67be348988389f8
unknown <antonio.angelini@rrdl.it>
2021-05-13 10:40:20 +0200

    prova repo

--- LetsJam/letsjam/src/main/java/it/univaq/disim/mwt/letsjam/LetsjamApplication.java
+++ LetsJam/letsjam/src/main/java/it/univaq/disim/mwt/letsjam/LetsjamApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LetsjamApplication {
-
+	//viva la mamma
 	public static void main(String[] args) {
 		SpringApplication.run(LetsjamApplication.class, args);
 	}
