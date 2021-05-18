af80411d854f81050efea917332cebd0595f4987
Cico96 <40207930+Cico96@users.noreply.github.com>
2021-05-18 21:54:43 +0200

    Fix path project

--- src/main/java/it/univaq/disim/mwt/letsjam/domain/Brano.java
+++ src/main/java/it/univaq/disim/mwt/letsjam/domain/Brano.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Brano {
-	//Test testabile testato con la testa testardosa
+	//Test testabile testato con la testa testardosa tutto apposto a ferragosto
 	private Long id_brano;
 	private String autore;
 	private String titolo;
