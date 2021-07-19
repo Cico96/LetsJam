b6644aed946e8216caed8d0f2a09dfa581d2a9c9
Cico96 <40207930+Cico96@users.noreply.github.com>
2021-07-19 17:28:42 +0200

    spartito service

---
+++ src/main/java/it/univaq/disim/mwt/letsjam/business/SpartitoService.java
@@ -0,0 +1,25 @@
+package it.univaq.disim.mwt.letsjam.business;
+
+import it.univaq.disim.mwt.letsjam.domain.Spartito;
+import it.univaq.disim.mwt.letsjam.exceptions.BusinessException;
+
+public interface SpartitoService {
+	
+	Spartito findSpartitoByTitolo(String titolo) throws BusinessException;
+	
+	Spartito findSpartitoByContento (String contenuto) throws BusinessException;
+	
+	Spartito findSpartitoByVerificato(String titolo) throws BusinessException;
+	
+	void addLike(Spartito spartito, Long id_utente) throws BusinessException;
+	
+	void update(Spartito spartito) throws BusinessException;
+	
+	void deleteSpartitoById (Long id) throws BusinessException;
+	
+	Spartito insert(Spartito spartito) throws BusinessException;
+	
+	void save(Spartito spartito) throws BusinessException;
+	
+	//metodo insert e update hanno bisogno di un id utente?
+}
