63e19f9a4cead2f334483b72ec7b25fd4ea404b9
Antonio <antonioang9@gmail.com>
2021-07-24 14:24:42 +0200

    Realizzazione dummy controller, divisione del layout con Thymeleaf, creazione pagina di errore

--- src/main/java/it/univaq/disim/mwt/letsjam/presentation/DummyController.java
+++ src/main/java/it/univaq/disim/mwt/letsjam/presentation/DummyController.java
@@ -3,13 +3,26 @@ package it.univaq.disim.mwt.letsjam.presentation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HomeController {
+@RequestMapping("/")
+public class DummyController {
     @GetMapping("/")
     public String home(@RequestParam(name="name", required=false, defaultValue="Jhon Chiccovic") String name, Model model){
         model.addAttribute("name", name);
-        return "home";
+        return "home/home";
     }
+    
+    @GetMapping("/error")
+    public String error(Model model){
+        return "common/error";
+    }
+    
+    @GetMapping("/forbidden")
+    public String forbidden(Model model){
+        return "common/forbidden";
+    }
+
 }
