e3500a33c3ba6a42b7ab79881c3486c2ed3ab9c3
Mattia Lenza <m.lenza@nrgsys.it>
2021-08-05 22:09:09 +0200

    Added entity jpa annotations

--- src/main/java/it/univaq/disim/mwt/letsjam/domain/Commento.java
+++ src/main/java/it/univaq/disim/mwt/letsjam/domain/Commento.java
@@ -2,18 +2,38 @@ package it.univaq.disim.mwt.letsjam.domain;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
+import it.univaq.disim.mwt.letsjam.presentation.validation.OnCreate;
+import it.univaq.disim.mwt.letsjam.presentation.validation.OnUpdate;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "commenti")
+@NoArgsConstructor
 @Getter
 @Setter
-public class Commento {
+public class Commento extends AbstractPersistableEntity{
 	
-	private Long id;
+	@NotEmpty(groups ={OnCreate.class, Default.class})
+	@Size(min = 3, max = 500, groups = {OnCreate.class, OnUpdate.class, Default.class})
 	private String contenuto;
-	private LocalDate data;
+	
+	@OneToOne
 	private Commento commentoPadre;
+	
+	@OneToOne
 	private Spartito spartito;
+	
+	@OneToOne
 	private Utente utente;
-
+	
 }
--- src/main/java/it/univaq/disim/mwt/letsjam/domain/Strumento.java
+++ src/main/java/it/univaq/disim/mwt/letsjam/domain/Strumento.java
@@ -1,14 +1,32 @@
 package it.univaq.disim.mwt.letsjam.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.validation.constraints.Size;
+
+import it.univaq.disim.mwt.letsjam.presentation.validation.OnCreate;
+import it.univaq.disim.mwt.letsjam.presentation.validation.OnUpdate;
+
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
+@Entity
+@Table(name = "strumenti")
+@NoArgsConstructor
 @Getter
 @Setter
-public class Strumento {
+public class Strumento extends AbstractPersistableEntity {
 	
-	private Long id;
+	@NotEmpty(groups ={OnCreate.class, Default.class})
+	@Size(min = 3, max = 50, groups = {OnCreate.class, OnUpdate.class, Default.class})
 	private String nome;
+
+	@NotEmpty(groups ={OnCreate.class, Default.class})
+	@Size(min = 3, max = 50, groups = {OnCreate.class, OnUpdate.class, Default.class})
 	private String chiave;
 
 }
