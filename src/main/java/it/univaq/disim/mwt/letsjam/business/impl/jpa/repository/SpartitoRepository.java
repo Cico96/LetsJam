c0ef2afdbbbaf6811b7a65cd99c62a5607f75a13
Cico96 <40207930+Cico96@users.noreply.github.com>
2021-08-26 17:58:49 +0200

    fix

--- src/main/java/it/univaq/disim/mwt/letsjam/business/impl/jpa/repository/SpartitoRepository.java
+++ src/main/java/it/univaq/disim/mwt/letsjam/business/impl/jpa/repository/SpartitoRepository.java
@@ -3,7 +3,6 @@ package it.univaq.disim.mwt.letsjam.business.impl.jpa.repository;
 import org.springframework.stereotype.Repository;
 import it.univaq.disim.mwt.letsjam.domain.Spartito;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
