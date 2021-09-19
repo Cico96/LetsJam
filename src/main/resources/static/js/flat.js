9aa9197860350068c4d993ea7bbdeefb23981004
Antonio <antonioang9@gmail.com>
2021-09-19 17:48:30 +0200

    ui crea/carica(da ultimare), chiamata reperimento dati spartito, modale scelta crea/carica

--- src/main/resources/static/js/flat.js
+++ src/main/resources/static/js/flat.js
@@ -1,20 +1,100 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    document.getElementById('file-input').addEventListener('change', readSingleFile, false);
+
+    // Create embed in the `embed-example` div. By default the embed will fit its container
+    let container = document.getElementById("embed-example");
+    console.log(container);
+    let embed = new Flat.Embed(container, {
+        // The score hosted on Flat we use here as template.
+        // You can also use `embed.loadMusicXML(score)` to load your MsuicXML on the fly:
+        // https://flat.io/developers/docs/embed/javascript.html#loadmusicxmlscore-mixed-promisevoid-error
+        score: "",
+        // The embed configuration parameters
+        height: "500px",
+        embedParams: {
+            mode: "edit",
+            appId: "59e7684b476cba39490801c2",
+            // Customization: https://flat.io/developers/docs/embed/url-parameters.html
+            branding: false,
+            controlsPosition: "top"
+        }
+    });
+
+    // showEditor();
+
+    document.getElementById('file-input').addEventListener('change', (e) => {
+        uploadFile(e,embed)
+    }, false);
+    chooseIfCreateOrUpload();
 });
 
-function readSingleFile(e) {
-    console.log('sono nel read single file');
-    var file = e.target.files[0];
+function chooseIfCreateOrUpload() {
+    let selectedOption = 'crea';
+
+    let alert = document.createElement('div')
+    let title = document.createElement('h3')
+    title.innerHTML='Scegli cosa fare'
+
+    let content = document.createElement('div')
+    let select = document.createElement('select')
+    select.classList.add('slct')
+    let optionCrea = document.createElement('option')
+    optionCrea.value = 'crea'
+    optionCrea.innerHTML = 'crea'
+    optionCrea.classList.add('opt')
+    let optionCarica = document.createElement('option')
+    optionCarica.classList.add('opt')
+    optionCarica.value = 'carica'
+    optionCarica.innerHTML = 'carica'
+
+    select.addEventListener('change', () => {
+        selectedOption = select.value;
+    })
+
+    let ConfirmButton = document.createElement('button')
+    ConfirmButton.classList.add('create-btn')
+    ConfirmButton.innerHTML= 'conferma'
+    ConfirmButton.addEventListener('click', () => {
+        alert.style.display = 'none'
+        if (selectedOption === 'carica') {
+            let nodeToDisplay = document.getElementById('create')
+            nodeToDisplay.style.visibility = 'visible'
+        } else if (selectedOption === 'crea') {
+            let nodeToDisplay = document.getElementById('upload')
+            nodeToDisplay.style.display= 'flex'
+        }
+    })
+
+    alert.classList.add('choose-alert')
+
+
+    select.appendChild(optionCrea)
+    select.appendChild(optionCarica)
+    content.appendChild(select)
+    alert.appendChild(title)
+    alert.appendChild(content)
+    alert.appendChild(ConfirmButton)
+
+    let parent = document.getElementById('flat-wrap')
+    parent.appendChild(alert)
+
+    return selectedOption;
+
+}
+
+function uploadFile(e, embed) {
+
+    let file = e.target.files[0];
     if (!file) {
         return;
     }
-    var reader = new FileReader();
+
+    let reader = new FileReader();
     reader.onload = function(e) {
-        var contents = e.target.result;
+        let contents = e.target.result;
         // Create embed in the `embed-example` div. By default the embed will fit its container
-        var container = document.getElementById("embed-example");
+        let container = document.getElementById("embed-example");
         console.log(container);
-        var embed = new Flat.Embed(container, {
+        let embed = new Flat.Embed(container, {
             // The score hosted on Flat we use here as template.
             // You can also use `embed.loadMusicXML(score)` to load your MsuicXML on the fly:
             // https://flat.io/developers/docs/embed/javascript.html#loadmusicxmlscore-mixed-promisevoid-error
@@ -30,18 +110,33 @@ function readSingleFile(e) {
             }
         });
 
-        //embed.loadMusicXML(contents);
-        embed.loadJSON(contents);
+        embed.loadMusicXML(contents);
+        // embed.loadJSON(contents);
         console.log(contents);
 
-        // Helper function to force downloading the exported file
-        var exportFile = function(buffer, mimeType, ext) {
-        var blobUrl = window.URL.createObjectURL(
-            new Blob([buffer], {
-            type: mimeType
+        embed.on('scoreLoaded', () => {
+            embed.getJSON().then(async (score) => {
+                let formData = new FormData();
+                formData.append("score", JSON.stringify(score));
+                return await fetch('/musicsheets/analyze', {
+                    method: "POST",
+                    ContentType: "application/json",
+                    body: formData
+                }).then((response) => {
+                    return response.json()
+                }).then((data) => {
+                    console.log(data)
+                });
             })
-        );
-        var a = document.createElement("a");
+        })
+
+        let exportFile = function(buffer, mimeType, ext) {
+            var blobUrl = window.URL.createObjectURL(
+                new Blob([buffer], {
+                    type: mimeType
+                })
+            );
+            var a = document.createElement("a");
             a.href = blobUrl;
             a.download = "exported-score." + ext;
             document.body.appendChild(a);
@@ -50,29 +145,19 @@ function readSingleFile(e) {
             a.remove();
         };
 
-        // Export as MusicXML when the button is clicked
-        // https://flat.io/developers/docs/embed/javascript.html#getmusicxmloptions-object-promisestringuint8array-error
         document.getElementById("export-xml").addEventListener("click", function() {
-        embed.getMusicXML({ compressed: true }).then(function(buffer) {
-            exportFile(buffer, "application/vnd.recordare.musicxml+xml", "mxl");
-        });
+            embed.getMusicXML({ compressed: true }).then(function(buffer) {
+                exportFile(buffer, "application/vnd.recordare.musicxml+xml", "mxl");
+            });
         });
 
-        // Export as MIDI when the button is clicked
-        // https://flat.io/developers/docs/embed/javascript.html#getmidi-promiseuint8array-error
         document.getElementById("export-pdf").addEventListener("click", function() {
             embed.print()
         });
 
-        // Export as PNG when the button is clicked
-        // https://flat.io/developers/docs/embed/javascript.html#getpngoptions-object-promisestringuint8array-error
         document.getElementById("export-png").addEventListener("click", function() {
-            /*embed.getPNG().then(function(buffer) {
-                exportFile(buffer, "image/png", "png");
-            });*/
-
             embed.getJSON().then(function (data) {
-            //console.log(JSON.stringify(data));
+                //console.log(JSON.stringify(data));
                 console.log(data);
                 //Scorro le parti
                 let parti = data["score-partwise"].part;
@@ -101,5 +186,7 @@ function readSingleFile(e) {
         });
     };
     reader.readAsText(file);
-}
 
+
+
+}
