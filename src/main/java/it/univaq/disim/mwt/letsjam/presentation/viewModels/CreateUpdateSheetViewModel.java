aec302262b1b3eb111967defc37737a27bffcf09
Antonio <antonioang9@gmail.com>
2021-09-21 00:37:43 +0200

    Create viewModel(da completare)

---
+++ src/main/java/it/univaq/disim/mwt/letsjam/presentation/viewModels/CreateUpdateSheetViewModel.java
@@ -0,0 +1,19 @@
+package it.univaq.disim.mwt.letsjam.presentation.viewModels;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateUpdateSheetViewModel {
+
+    String author;
+
+    String brano;
+
+    String title;
+
+    //content
+}
