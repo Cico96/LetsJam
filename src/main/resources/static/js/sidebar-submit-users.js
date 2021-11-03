791dc007b048f3877aa2f47a92bb659384e3622e
Antonio <antonioang9@gmail.com>
2021-11-04 00:35:52 +0100

    termine UI gestione utenti admin(mancano piccoli ritocchi)

--- src/main/resources/static/js/sidebar-submit-users.js
+++ src/main/resources/static/js/sidebar-submit-users.js
@@ -1,28 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let wrapper = document.getElementById('newAdmins')
-    let amichetti = document.querySelectorAll('[data-user]')
+    let wrapperPromote = document.getElementById('newAdmins')
+    let wrapperDelete = document.getElementById('deleteAdmins')
+    let amichetti = document.querySelectorAll('.user-card')
+    let users = [];
     amichetti.forEach((amico) => {
-        amico.parentNode.parentNode.addEventListener('click', function () {
-            let newAdmin = document.createElement('input')
-            newAdmin.style.height= '40px'
-            newAdmin.style.width= '40px'
-            newAdmin.style.color= '#ffffff'
-            let div = document.createElement('div')
-            div.style.paddingTop = '16px'
-            div.style.display = 'flex'
-            div.style.alignItems = 'center'
-            let label = document.createElement('label')
-            label.innerHTML = amico.previousElementSibling.innerHTML;
-            label.style.color = '#ffffff'
-            label.style.marginLeft = '24px'
-            newAdmin.setAttribute('type', 'checkbox')
-            newAdmin.setAttribute('value', amico.dataset.user)
-            newAdmin.setAttribute('checked', 'true')
-            div.appendChild(newAdmin);
-            div.appendChild(label);
-            wrapper.appendChild(div)
-            console.log(newAdmin)
+        amico.querySelector('#deleteUser').addEventListener('click', function (e) {
+            fillForm(wrapperDelete, users, e)
+        })
+        amico.querySelector('#promoteUser').addEventListener('click', function (e) {
+            fillForm(wrapperPromote, users, e)
         })
     })
-
 });
+
+function fillForm(wrapper, users, amico) {
+    if (users.includes(amico.currentTarget.dataset.user)) {
+        return
+    } else {
+        users.push(amico.currentTarget.dataset.user)
+    }
+    let newAdmin = document.createElement('input')
+    newAdmin.style.height = '40px'
+    newAdmin.style.width = '40px'
+    newAdmin.style.color = '#ffffff'
+    let div = document.createElement('div')
+    div.style.paddingTop = '16px'
+    div.style.display = 'flex'
+    div.style.alignItems = 'center'
+    let label = document.createElement('label')
+    label.innerHTML = amico.currentTarget.dataset.username;
+    label.style.color = '#ffffff'
+    label.style.marginLeft = '24px'
+    newAdmin.setAttribute('type', 'checkbox')
+    newAdmin.setAttribute('value', amico.currentTarget.dataset.user)
+    newAdmin.setAttribute('checked', 'true')
+    div.appendChild(newAdmin);
+    div.appendChild(label);
+    wrapper.appendChild(div)
+    console.log(newAdmin.value)
+}
