ff474b6f1b8b79719a03777ba327aa234c5d0c8a
Mattia Lenza <m.lenza@nrgsys.it>
2021-11-06 13:25:13 +0100

    Animated musicSheet comments with js

--- src/main/resources/static/js/musicsheet.js
+++ src/main/resources/static/js/musicsheet.js
@@ -79,7 +79,23 @@ document.addEventListener('DOMContentLoaded', ()=>{
         editor.print();
     });
 
+    document.querySelectorAll('.respond-button').forEach(b =>{
+        b.addEventListener('click',e =>{
+            e.preventDefault();
+            console.log(e.target.parentElement.parentElement.parentElement);
+            addRespondtextBox(e.target.parentElement.parentElement.parentElement);
+        });
+    });
 
+    document.querySelectorAll('.write-comment input').forEach(el =>{
+        el.addEventListener('keyup', (e)=>{
+            if(e.keyCode == 13){
+                let lastComment = document.querySelector('.comments-container').children;
+                lastComment = lastComment[lastComment.length-2];
+                addComment(el, lastComment, false);
+            }
+        })
+    });
 });
 
 var exportFile = function(buffer, mimeType, ext) {
@@ -96,3 +112,74 @@ var exportFile = function(buffer, mimeType, ext) {
     a.click();
     a.remove();
 };
+
+
+function addRespondtextBox(el){
+    document.querySelectorAll('.write-comment').forEach(el => el.style.display='none');
+    let container = document.createElement('div');
+    container.classList.add('write-comment');
+    container.style.marginLeft='7%';
+    let userImage = document.createElement('div');
+    userImage.classList.add('user-image');
+    userImage.append("\u00A0");
+    let input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('placeholder', 'Scrivi il tuo commento qui');
+    input.style.flex='17';
+    input.addEventListener('keyup', e =>{
+        if(e.keyCode === 13){
+            addComment(input, el, true);
+        }
+    });
+    container.appendChild(userImage);
+    container.appendChild(input);
+
+    el.parentNode.insertBefore(container, el.nextSibling);
+}
+
+function addComment(input, parent, isResponse){
+
+    let comment = document.createElement('div');
+    comment.classList = (isResponse) ? 'comment response d-flex align-items-center justify-content-start' : 'comment d-flex align-items-center justify-content-start';
+    let userImage = document.createElement('div');
+    userImage.classList.add('user-image');
+    userImage.append("\u00A0");
+    let textContainer = document.createElement('div');
+    textContainer.classList='d-flex flex-column align-items-start justify-content-start';
+    textContainer.style.flex='17';
+    let username = document.createElement('h6');
+    username.classList = 'user-name';
+    username.innerText= 'Fracchicco Sfranzi';
+    let p = document.createElement('p');
+    p.classList = 'comment-content';
+    p.innerText = input.value;
+
+    let commentActions=document.createElement('div');
+    commentActions.classList='d-flex align-items-center justify-content-start comment-actions';
+    commentActions.style.gap='10px';
+
+    let respond = document.createElement('a');
+    respond.classList='respond-button';
+    respond.innerText="Rispondi";
+    respond.href='';
+    respond.addEventListener('click',e =>{
+        e.preventDefault();
+        console.log(e.target.parentElement.parentElement.parentElement);
+        addRespondtextBox(e.target.parentElement.parentElement.parentElement);
+    });
+
+    commentActions.appendChild(respond);
+
+    textContainer.appendChild(username);
+    textContainer.appendChild(p);
+    textContainer.appendChild(commentActions);
+
+    comment.appendChild(userImage);
+    comment.appendChild(textContainer);
+
+    if(isResponse) input.parentElement.remove();
+    else input.parentElement.style.display='none';
+    input.value='';
+    parent.parentNode.insertBefore(comment, parent.nextSibling);
+    document.querySelectorAll('.write-comment').forEach(el => el.style.display='flex');
+}
