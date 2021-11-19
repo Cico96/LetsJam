f89df4a8845e1db93350b60a2ed356b0006bf44e
Antonio <antonioang9@gmail.com>
2021-11-19 21:35:52 +0100

    Piccolo fix sull'user role amministratore

--- src/main/java/it/univaq/disim/mwt/letsjam/presentation/GeneralController.java
+++ src/main/java/it/univaq/disim/mwt/letsjam/presentation/GeneralController.java
@@ -2,6 +2,7 @@ package it.univaq.disim.mwt.letsjam.presentation;
 
 import it.univaq.disim.mwt.letsjam.business.*;
 import it.univaq.disim.mwt.letsjam.domain.Genre;
+import it.univaq.disim.mwt.letsjam.security.UserRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -35,6 +36,11 @@ public class GeneralController {
         if(authentication !=null){
             //Logged
             User loggedUser = ((CustomUserDetails) authentication.getPrincipal()).getUser();
+
+            if(loggedUser.getRole().equals(UserRoles.AMMINISTRATORE)){
+                return "home/adminPanel";
+            }
+
             System.out.println(loggedUser.getFirstname()+" "+loggedUser.getLastname() + " "+loggedUser.getRole());
 
             List<MusicSheet> latest = spartitoService.getLastInsertMusicSheets();
@@ -49,17 +55,6 @@ public class GeneralController {
         model.addAttribute("musicSheetByGenre", musicSheetByGenre);
         return "home/home";
     }
-
-    @GetMapping("admin/home")
-    public String adminHome(Model model, Authentication authentication){
-        if(authentication !=null){
-            //Logged
-            User loggedUser = ((CustomUserDetails) authentication.getPrincipal()).getUser();
-            return "home/adminPanel";
-        }
-        return "common/forbidden";
-    }
-	
     
     @GetMapping("forbidden")
     public String forbidden(Model model){
